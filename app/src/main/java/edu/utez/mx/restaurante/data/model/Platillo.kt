8e9ccd796a395622680b12c9d0ff7f247d990111
jair <jair.vasquez@omnicloud.mx>
2025-11-09 21:07:18 -0600

    Creacion de las tablas

--- app/src/main/java/edu/utez/mx/restaurante/data/model/Platillo.kt
+++ app/src/main/java/edu/utez/mx/restaurante/data/model/Platillo.kt
@@ -1,11 +1,14 @@
 package edu.utez.mx.restaurante.data.model
 
 import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class Platillo (
-    val id: Int,
-    @DrawableRes val image: Int,
+@Entity(tableName = "platillos")
+data class Platillo(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @DrawableRes val image: Int = 0,
     val nombre: String,
-    val descripcion: String,
+    val descripcion: String?,   // opcional para permitir vacío
     val precio: Double
 )
